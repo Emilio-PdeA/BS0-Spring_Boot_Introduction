5181ac6bf1f0b68a219d2bcc003dc6e5d073f6f0
emilio.perez <emilio.pdea@gmail.com>
2022-04-21 13:00:59 +0200

    Add Comments

--- bosonit/src/main/java/com/example/bosonit/UserController.java
+++ bosonit/src/main/java/com/example/bosonit/UserController.java
@@ -9,12 +9,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
+    /*
+    * METHOD: GET
+    * Test URL: localhost:8080/user/{name}
+    * Response: "Hello, {name}"
+    */
     private static final String template = "Hello, %s";
     @GetMapping("/user/{name}")
     public String getUser(@PathVariable String name){
         return String.format(template,name);
     }
 
+    /*
+    * METHOD: POST
+    * Test URL: localhost:8080/useradd
+    * BODY (JSON): {"name":$name, "city":$city, "age":$age}
+    * RESPONSE: {"name":$name, "city":$city, "age":$age + 1}
+    * */
     @PostMapping(path="/useradd", consumes = "application/json")
     public ResponseEntity<String> postUser(@RequestBody User user) {
         user.setAge(user.getAge()+1);
